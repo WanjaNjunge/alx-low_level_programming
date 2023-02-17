270ae81e52df070c4698dbcc768aa4c875fb7d19
WanjaNjunge <wnjunge19@gmail.com>
2023-02-16 16:17:31 -0800

    edit file 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
  * main - program that prints line to standard error
@@ -6,6 +5,6 @@
  */
 int main(void)
 {
-fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
